2b95ea5ccbcd8e97bda62818f851958b69fb3035
Paul Holden <paul@holdensoftware.com>
2019-11-12 08:11:19 -0800

    Load reporters

--- lib/bigsister/configuration.rb
+++ lib/bigsister/configuration.rb
@@ -1,17 +1,21 @@
 require "bigsister/monitors/local"
 require "bigsister/monitors/springcm"
+require "bigsister/reporters/csv_reporter"
 
 module BigSister
   class Configuration
     MONITOR_TYPES = %w(local springcm).freeze
+    REPORTER_TYPES = %w(csv).freeze
 
-    attr_reader :monitors
+    attr_reader :monitors, :reporters
 
     def initialize(data)
       @monitors = []
+      @reporters = []
       @config_errors = []
 
       load_monitoring(data)
+      load_reporting(data)
     end
 
     def valid?
@@ -40,6 +44,27 @@ module BigSister
         BigSister::LocalMonitor.new(location, i)
       elsif type == "springcm"
         BigSister::SpringcmMonitor.new(location, i)
+      end
+    end
+
+    def load_reporting(data)
+      @reporting = data.fetch("report", [])
+      @reporting.each_with_index { |reporter, i|
+        # Validate reporting type
+        type = reporter.fetch("type", nil)
+        if type.nil?
+          config_error("No report type specified for reporter #{i}")
+        elsif !REPORTER_TYPES.include?(type)
+          config_error("Invalid report type specifeid for reporter #{i}")
+        end
+        @reporters.push(load_reporter(type, reporter, i))
+        @reporters = @reporters.reject(&:nil?)
+      }
+    end
+
+    def load_reporter(type, reporter, i)
+      if type == "csv"
+        BigSister::CsvReporter.new(reporter, i)
       end
     end
 
